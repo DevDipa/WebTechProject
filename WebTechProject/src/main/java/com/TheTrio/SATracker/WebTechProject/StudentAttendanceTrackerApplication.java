a986b4d6741febe618a1dfd2bdf2d84e585dceae
Dipa <tamangdipa77@gmail.com>
2025-11-06 23:05:46 +0545

    Spring Boot's been initialized!

---
+++ WebTechProject/src/main/java/com/TheTrio/SATracker/WebTechProject/StudentAttendanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.TheTrio.SATracker.WebTechProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAttendanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAttendanceTrackerApplication.class, args);
+	}
+
+}
---
+++ WebTechProject/src/test/java/com/TheTrio/SATracker/WebTechProject/StudentAttendanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TheTrio.SATracker.WebTechProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAttendanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
